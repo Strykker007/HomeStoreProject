05782be297a1fcc8a87d3dfd9f202dbdfbedbb35
Tiago Lima <tiago433@hotmail.com>
2021-08-24 11:03:17 -0300

    second commit

---
+++ lib/models/stores_manager.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:lojavirtual2/models/store.dart';
+
+class StoresManager extends ChangeNotifier {
+
+  StoresManager(){
+    _checkChanges();
+    _loadStoreList();
+    _startTimer();
+
+  }
+
+  List<Store> stores = [];
+
+  Timer _timer;
+
+  final Firestore firestore = Firestore.instance;
+
+  Future<void> _loadStoreList() async {
+    final snapshot = await firestore.collection('stores').getDocuments();
+
+    stores = snapshot.documents.map((e) => Store.fromDocument(e)).toList();
+
+    notifyListeners();
+  }
+
+  void _startTimer(){
+    _timer = Timer.periodic(const Duration(minutes: 1), (timer) {
+      _checkOpening();
+    });
+  }
+
+  void _checkOpening(){
+    for(final store in stores){
+      store.updateStatus();
+    }
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer?.cancel();
+  }
+
+  void _checkChanges(){
+
+    Firestore.instance.collection("stores").snapshots().listen((snapshot){
+      snapshot.documentChanges.forEach((change){
+
+        switch(change.type){
+
+          case DocumentChangeType.modified:
+            _loadStoreList();
+            notifyListeners();
+            break;
+          case DocumentChangeType.added:
+            break;
+          case DocumentChangeType.removed:
+            break;
+        }
+      });
+
+    });
+
+  }
+}
